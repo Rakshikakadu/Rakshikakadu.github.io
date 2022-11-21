3e303f41d8a60ea8553eeed2e5440c4efc8af876
Rakshikakadu <rakshikakadu@gmail.com>
2022-11-21 21:32:50 +0530

    profile pic updated

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
   
     // typing text animation script
     var typed = new Typed(".typing", {
-      strings: [" A Java Back-End Developer"],
+      strings: [" Aspiring Java Back-End Developer"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true,
     });
   
     var typed = new Typed(".typing-2", {
-      strings: ["A Java Back-End Developer"],
+      strings: [" Aspiring Java Back-End Developer"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true,
