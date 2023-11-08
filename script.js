3644a63305dec3b5b31d0899d8e4f620c46527ab
Rakshikakadu <rakshikakadu@gmail.com>
2023-11-08 16:08:16 +0530

    update resume

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
   
     // typing text animation script
     var typed = new Typed(".typing", {
-      strings: [" Aspiring Java Back-End Developer"],
+      strings: [" Java Back-End Developer"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true,
     });
   
     var typed = new Typed(".typing-2", {
-      strings: [" Aspiring Java Back-End Developer"],
+      strings: [" Java Back-End Developer"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true,
